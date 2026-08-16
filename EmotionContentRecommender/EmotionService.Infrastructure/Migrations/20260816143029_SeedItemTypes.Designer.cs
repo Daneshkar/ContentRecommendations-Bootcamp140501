0d01d357a00f11d2d0f3a2dfc64f05f0f13e89cb
ColdTears619 <a.hate61947@gmail.com>
2026-08-16 18:27:33 +0330

    feat(emotion): seed default ItemTypes

--- EmotionContentRecommender/EmotionService.Infrastructure/Migrations/20260816143029_SeedItemTypes.Designer.cs
+++ EmotionContentRecommender/EmotionService.Infrastructure/Migrations/20260816143029_SeedItemTypes.Designer.cs
@@ -4,6 +4,7 @@ using EmotionService.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmotionService.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260816143029_SeedItemTypes")]
+    partial class SeedItemTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmotionContentRecommender/EmotionService.Infrastructure/Migrations/20260816143029_SeedItemTypes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EmotionService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedItemTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ItemTypes",
+                columns: new[] { "Id", "IsActive", "Name" },
+                values: new object[,]
+                {
+                    { 1, true, "Movie" },
+                    { 2, true, "Game" },
+                    { 3, true, "Book" },
+                    { 4, true, "Music" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ItemTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ItemTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ItemTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ItemTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
